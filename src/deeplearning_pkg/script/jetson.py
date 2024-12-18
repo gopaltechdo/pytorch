84d4fa160a7c010e4432d697a9b89ecad701a413
gopal <gopalsingh.g60@gmail.com>
2024-12-18 13:48:27 +0530

    tensor

--- src/deeplearning_pkg/script/jetson.py
+++ src/deeplearning_pkg/script/jetson.py
@@ -22,3 +22,22 @@ print(random_tensor.ndim)
 #create  a random  with similar shape to an image tensor 
 
 random_image_size_tensor = torch.rand(3,224,224)  #  colour channel height, & weidth 
+
+#creating tensor of all zeroes
+zeroes = torch.zeros(3,4)
+print(zeroes)
+
+#creating tensor of all ones
+ones = torch.ones(3,4)
+print(ones)
+#use torch range
+
+one_to_ten= (torch.arange(1,11))
+print(one_to_ten)
+
+range =torch.arange(start=0, end=1000, step=77)
+print(range)
+
+#creating tesors likes
+ten_zeros= torch.zeros_like(input=one_to_ten)
+print(ten_zeros)
